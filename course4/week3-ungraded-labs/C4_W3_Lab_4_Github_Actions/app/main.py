62c9d284e20446780137e51a16dd2eb1fba16a72
Gavin Crasta <58162538+Gavin0203@users.noreply.github.com>
2023-03-24 17:06:10 +0100

    Testing the CI/CD pipeline

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -4,7 +4,7 @@ from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
-
+#conlist : Type method for constraining lists.
 
 app = FastAPI(title="Predicting Wine Class with batching")
 
